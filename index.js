ea8d688461d7e2acaefa0a2df173eabff541d736
Egor Grinevich <egor.grinevich1999@gmail.com>
2019-10-24 01:43:34 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 /* eslint-disable space-before-blocks */
-document.querySelector('.team_message1').classList.add('hidd');
-document.querySelector('.team_message2').classList.add('hidd');
-document.querySelector('.team_message3').classList.add('hidd');
-document.querySelector('.team_message4').classList.add('hidd');
-document.querySelector('.team_message5').classList.add('hidd');
-document.querySelector('.team_message6').classList.add('hidd');
-
 
 document.querySelector('.team_photo1').onclick = () => {
   const b = document.querySelector('.team_message1');
   if (b.classList.contains('hidd')) b.classList.remove('hidd'); else b.classList.add('hidd');
 };
-
 
 document.querySelector('.team_photo2').onclick = () => {
   const b = document.querySelector('.team_message2');
@@ -43,55 +35,51 @@ const sq = document.querySelectorAll('.square1');
 let currentSlide = 0;
 
 document.getElementById('prev').onclick = function nextSlide() {
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'pic_size slide';
   sq[currentSlide].className = 'square1';
   currentSlide = (currentSlide - 1) % slides.length;
   if (currentSlide < 0) currentSlide = slides.length - 1;
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'pic_size slide showing';
   sq[currentSlide].className = 'square1 chosen_sq';
 };
 
 document.getElementById('next').onclick = function nextSlide() {
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'pic_size slide';
   sq[currentSlide].className = 'square1';
   currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'pic_size slide showing';
   sq[currentSlide].className = 'square1 chosen_sq';
 };
 
 document.querySelector('.sq1').onclick = () => {
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'pic_size slide';
   sq[currentSlide].className = 'square1';
   currentSlide = 0;
-  slides[currentSlide].className = 'slide showing';
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'pic_size slide showing';
   sq[currentSlide].className = 'square1 sq1 chosen_sq';
 };
 
 document.querySelector('.sq2').onclick = () => {
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'pic_size slide';
   sq[currentSlide].className = 'square1';
   currentSlide = 1;
-  slides[currentSlide].className = 'slide showing';
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'pic_size slide showing';
   sq[currentSlide].className = 'square1 sq2 chosen_sq';
 };
 
 document.querySelector('.sq3').onclick = () => {
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'pic_size slide';
   sq[currentSlide].className = 'square1';
   currentSlide = 2;
-  slides[currentSlide].className = 'slide showing';
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'pic_size slide showing';
   sq[currentSlide].className = 'square1 sq3 chosen_sq';
 };
 
 document.querySelector('.sq4').onclick = () => {
-  slides[currentSlide].className = 'slide';
+  slides[currentSlide].className = 'pic_size slide';
   sq[currentSlide].className = 'square1';
   currentSlide = 3;
-  slides[currentSlide].className = 'slide showing';
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'pic_size slide showing';
   sq[currentSlide].className = 'square1 sq4 chosen_sq';
 };
 
@@ -101,9 +89,9 @@ document.querySelector('.square6').onclick = function show() {
   document.querySelector('.square8').className = 'square square8';
   document.querySelector('.square9').className = 'square square9';
   document.querySelector('.a1').className = 'apple_img1 a1 ';
-  document.querySelector('.a2').className = 'apple_img1 a2 apple_img4';
-  document.querySelector('.a3').className = 'apple_img1 a3 apple_img4';
-  document.querySelector('.a4').className = 'apple_img1 a4 apple_img4';
+  document.querySelector('.a2').className = 'apple_img1 a2 hide';
+  document.querySelector('.a3').className = 'apple_img1 a3 hide';
+  document.querySelector('.a4').className = 'apple_img1 a4 hide';
 };
 
 document.querySelector('.square7').onclick = function show() {
@@ -111,10 +99,10 @@ document.querySelector('.square7').onclick = function show() {
   document.querySelector('.square6').className = 'square square6';
   document.querySelector('.square8').className = 'square square8';
   document.querySelector('.square9').className = 'square square9';
-  document.querySelector('.a1').className = 'apple_img1 a1 apple_img4';
+  document.querySelector('.a1').className = 'apple_img1 a1 hide';
   document.querySelector('.a2').className = 'apple_img1 a2';
-  document.querySelector('.a3').className = 'apple_img1 a3 apple_img4';
-  document.querySelector('.a4').className = 'apple_img1 a4 apple_img4';
+  document.querySelector('.a3').className = 'apple_img1 a3 hide';
+  document.querySelector('.a4').className = 'apple_img1 a4 hide';
 };
 
 document.querySelector('.square8').onclick = function show() {
@@ -122,10 +110,10 @@ document.querySelector('.square8').onclick = function show() {
   document.querySelector('.square7').className = 'square square7';
   document.querySelector('.square6').className = 'square square6';
   document.querySelector('.square9').className = 'square square9';
-  document.querySelector('.a1').className = 'apple_img1 a1 apple_img4';
-  document.querySelector('.a2').className = 'apple_img1 a2 apple_img4';
+  document.querySelector('.a1').className = 'apple_img1 a1 hide';
+  document.querySelector('.a2').className = 'apple_img1 a2 hide';
   document.querySelector('.a3').className = 'apple_img1 a3';
-  document.querySelector('.a4').className = 'apple_img1 a4 apple_img4';
+  document.querySelector('.a4').className = 'apple_img1 a4 hide';
 };
 
 document.querySelector('.square9').onclick = function show() {
@@ -133,35 +121,54 @@ document.querySelector('.square9').onclick = function show() {
   document.querySelector('.square7').className = 'square square7';
   document.querySelector('.square8').className = 'square square8';
   document.querySelector('.square6').className = 'square square6';
-  document.querySelector('.a1').className = 'apple_img1 a1 apple_img4';
-  document.querySelector('.a2').className = 'apple_img1 a2 apple_img4';
-  document.querySelector('.a3').className = 'apple_img1 a3 apple_img4';
+  document.querySelector('.a1').className = 'apple_img1 a1 hide';
+  document.querySelector('.a2').className = 'apple_img1 a2 hide';
+  document.querySelector('.a3').className = 'apple_img1 a3 hide';
   document.querySelector('.a4').className = 'apple_img1 a4';
 };
 
 document.addEventListener('click', (event) => {
-  if (event.srcElement.classList.contains('arrow')){
-    const arr = event.path[2].classList.value;
+  let elements;
+  let change;
+  let flag = 1;
+
+  if (event.srcElement.classList.contains('know_more')) {
+    const arr = event.path[1].classList.value;
     const newStr = arr.replace(' ', '.');
     const vot = `.${newStr}`;
-    const elements = document.querySelectorAll(vot);
+    elements = document.querySelectorAll(vot);
+
+    const path1 = event.path[3].classList[1];
+    const path2 = event.path[2].className;
+
+    const change1 = `.${path1} .${path2}`;
+
+    change = document.querySelector(change1);
+  } else if (event.srcElement.classList.contains('arrow')) {
+    const arr = event.path[2].classList.value;
+
+    const newStr = arr.replace(' ', '.');
+    const vot = `.${newStr}`;
+    elements = document.querySelectorAll(vot);
 
     const path1 = event.path[4].classList[1];
     const path2 = event.path[3].className;
 
     const change1 = `.${path1} .${path2}`;
-    const change = document.querySelector(change1);
-
-    // eslint-disable-next-line no-restricted-syntax
+    change = document.querySelector(change1);
+  } else {
+    flag = 0;
+  }
+  if (flag === 1){
+  // eslint-disable-next-line no-restricted-syntax
     for (const elem of elements) {
-      if (elem.classList.contains('apple_img4')) {
-        elem.classList.remove('apple_img4');
+      if (elem.classList.contains('hide')) {
+        elem.classList.remove('hide');
         change.style.backgroundColor = '#71f5ea';
         change.style.color = 'black';
         change.style.opacity = '1';
-      }
-      else {
-        elem.classList.add('apple_img4');
+      } else {
+        elem.classList.add('hide');
         change.style.backgroundColor = 'black';
         change.style.opacity = '0.7';
         change.style.color = 'white';
